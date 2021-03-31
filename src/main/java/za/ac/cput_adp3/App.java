4163a64a22d0477611cd84a9146d45a430513541
Joshua Luke <214234169@mycput.ac.za>
2021-04-01 01:09:45 +0200

    Added a main() method

--- src/main/java/za/ac/cput_adp3/App.java
+++ src/main/java/za/ac/cput_adp3/App.java
@@ -6,8 +6,24 @@ package za.ac.cput_adp3;
  */
 public class App 
 {
+    /**
+     * This returns a string
+     * @param str
+     * @return
+     */
+    public int lengthOfString(String str)
+    {
+        if (str ==null || str.isEmpty()) return 0;
+        return str.length();
+    }
+
+
     public static void main( String[] args )
     {
         System.out.println( "ADP 3 -->Group Assignment" );
     }
+
+
+
+
 }
