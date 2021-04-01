c7238c5738656d8808ecad3e1ceb1f1ef0e55cd2
Joshua Luke <214234169@mycput.ac.za>
2021-04-01 19:58:09 +0200

    Renamed Test.java to Employee.java

---
+++ src/main/java/za/ac/cput_adp3/Employee.java
@@ -0,0 +1,12 @@
+package za.ac.cput_adp3;
+
+/**
+ * This is my small program
+ * @author 214234169
+ */
+public class Employee
+{
+
+
+}
+
--- src/main/java/za/ac/cput_adp3/Test.java
+++
@@ -1,6 +0,0 @@
-package za.ac.cput_adp3;
-
-public class Test
-{
-
-}
